5338d9c0b1a70484d7d879af276270907b5f784f
shakibapon1234-maker <shakibapon1234@gmail.com>
2026-09-20 12:41:13 +0600

    app launcher

--- start_desktop.bat
+++ start_desktop.bat
@@ -1,34 +1,35 @@
 @echo off
-title Photo & 3D Text Studio - Desktop Mode
+title "Photo & 3D Text Studio - Desktop Mode"
 cd /d "%~dp0"
 
-:: Locate Electron Binary Candidates
 set "ELECTRON_EXE="
 set "MAIN_JS=%~dp0main-electron.js"
 
-:: 1. Local node_modules (best)
 if exist "%~dp0node_modules\electron\dist\electron.exe" (
     set "ELECTRON_EXE=%~dp0node_modules\electron\dist\electron.exe"
-
-:: 2. App Launcher node_modules (G: drive)
-) else if exist "G:\all\app launcher\App-Launcher\node_modules\electron\dist\electron.exe" (
+    goto :HAVE_ELECTRON
+)
+if exist "G:\all\app launcher\App-Launcher\node_modules\electron\dist\electron.exe" (
     set "ELECTRON_EXE=G:\all\app launcher\App-Launcher\node_modules\electron\dist\electron.exe"
-
-:: 3. Video Editor node_modules (G: drive)
-) else if exist "G:\all\Video-Editor\node_modules\electron\dist\electron.exe" (
+    goto :HAVE_ELECTRON
+)
+if exist "G:\all\Video-Editor\node_modules\electron\dist\electron.exe" (
     set "ELECTRON_EXE=G:\all\Video-Editor\node_modules\electron\dist\electron.exe"
-
-:: 4. Sabre node_modules (G: drive)
-) else if exist "G:\all\SABRE\Sabre\node_modules\electron\dist\electron.exe" (
+    goto :HAVE_ELECTRON
+)
+if exist "G:\all\SABRE\Sabre\node_modules\electron\dist\electron.exe" (
     set "ELECTRON_EXE=G:\all\SABRE\Sabre\node_modules\electron\dist\electron.exe"
-
-:: 5. Galileo node_modules (G: drive)
-) else if exist "G:\all\GELELIO\Gellelio-training-mode\node_modules\electron\dist\electron.exe" (
+    goto :HAVE_ELECTRON
+)
+if exist "G:\all\GELELIO\Gellelio-training-mode\node_modules\electron\dist\electron.exe" (
     set "ELECTRON_EXE=G:\all\GELELIO\Gellelio-training-mode\node_modules\electron\dist\electron.exe"
+    goto :HAVE_ELECTRON
 )
 
+:HAVE_ELECTRON
+
 if exist "%ELECTRON_EXE%" (
-    echo [OK] Launching Photo & 3D Text Studio with Electron...
+    echo [OK] Launching Photo and 3D Text Studio with Electron...
     start "" "%ELECTRON_EXE%" "%MAIN_JS%"
     exit /b 0
 )
